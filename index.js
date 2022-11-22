b317feba35d289e4d21b1cf0af4141cb4bf3678f
edrisso <jrd1sso@gmail.com>
2022-11-22 13:51:03 +0200

    update main branch

--- index.js
+++
@@ -1,8 +0,0 @@
-const fname = prompt("Enter first name");
-const lname = prompt("Enter last name");
-const fullName = `${fname} ${lname}`;
-
-const birthYear = prompt("Enter year of birth");
-const age = new Date().getFullYear() - birthYear;
-
-document.write(`Welcome ${fullName}! you are ${age} years old.`);
