5e770c769aa7cd5848730743a1d6be1041479616
edriso <edriso.dev@gmail.com>
2023-01-17 14:20:41 +0200

    starter file

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ const app = express();
 
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(`${__dirname}/index.html`);
-});
-
-app.post("/calculator", (req, res) => {
-  const { num1, num2 } = req.body;
-
-  if (num1 && num2) {
-    const sum = Number(num1) + Number(num2);
-    res.status(200).send("Sum is " + sum);
-  } else {
-    res.status(404).send("Something went wrong!");
-  }
-});
-
 const port = 3000;
 app.listen(port, () =>
   console.log(`Server is up and running on port ${port}...`)
